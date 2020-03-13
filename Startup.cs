44f4fb001731e5c82153b943c77c4b6301ac19f6
Tatiane <tatiane.berrocal@gmail.com>
2020-03-13 19:51:55 -0300

    Revisão do projeto (Alteração do launchUrl)

--- Startup.cs
+++ Startup.cs
@@ -46,7 +46,6 @@ namespace CopaFilmes
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
             });
         }
     }
